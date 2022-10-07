3856e60e5705dbe19318a32c5af2bdb6c9e59fe8
Alejandro Antelo <alfasant@protonmail.com>
2022-10-07 15:28:53 +0200

    Added comments and made code more legible in activity4.js

--- js/activity4.js
+++ js/activity4.js
@@ -1,10 +1,17 @@
+// Create an array and initialize the values to 0
+let max_num = 10
 let num_arr = []
-for (i = 0; i < 10000; i++) {
-  let rand = parseInt(Math.random()*10) + 1
-  arr_rand = num_arr[rand-1]
-  num_arr[rand-1] = (isNaN(arr_rand) ? 0 : arr_rand) + 1
+for (let i = 0; i < max_num; i++) {
+  num_arr[i] = 0
 }
 
+// Store the counts in the array
+for (i = 0; i < 10000; i++) {
+  let rand = parseInt(Math.random()*max_num) + 1
+  num_arr[rand-1]++
+}
+
+// Print out the counts
 for (let i = 0; i < num_arr.length; i++) {
   document.write('Nº'+(1+i)+': '+num_arr[i]+'<br>')
 }
